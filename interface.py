e115327dd466dbd487fa99d06b66abdc72b83935
Jered <mcinerney.de@northeastern.edu>
2023-07-13 15:30:34 -0400

    added comparing

--- interface.py
+++ interface.py
@@ -10,33 +10,38 @@ from omegaconf import OmegaConf
 
 
 actor_checkpoints = {
-    'supervised_dirichlet': (
-        '/work/frink/mcinerney.de/ehr-diagnosis-agent/ehr_diagnosis_agent/wandb/run-20230604_010745-6xacw8w1/files/ckpt_epoch=8_updates=986.pt',
-        '/work/frink/mcinerney.de/ehr-diagnosis-agent/ehr_diagnosis_agent/wandb/run-20230604_010745-6xacw8w1/files/config.yaml'
-    ),
-    'supervised_beta': (
-        '/work/frink/mcinerney.de/ehr-diagnosis-agent/ehr_diagnosis_agent/wandb/run-20230614_095808-ov4baor2/files/ckpt_epoch=3_updates=34.pt',
-        '/work/frink/mcinerney.de/ehr-diagnosis-agent/ehr_diagnosis_agent/wandb/run-20230614_095808-ov4baor2/files/config.yaml'
-    ),
-    'supervised_beta_with_bias': (
-        '/work/frink/mcinerney.de/ehr-diagnosis-agent/ehr_diagnosis_agent/wandb/run-20230614_134807-aaf16kc6/files/ckpt_epoch=23_updates=289.pt',
-        '/work/frink/mcinerney.de/ehr-diagnosis-agent/ehr_diagnosis_agent/wandb/run-20230614_134807-aaf16kc6/files/config.yaml'
-    ),
-    'supervised_beta_with_attn': (
-        '/work/frink/mcinerney.de/ehr-diagnosis-agent/ehr_diagnosis_agent/wandb/run-20230614_183930-pj9ipm5h/files/ckpt_epoch=9_updates=92.pt',
-        '/work/frink/mcinerney.de/ehr-diagnosis-agent/ehr_diagnosis_agent/wandb/run-20230614_183930-pj9ipm5h/files/config.yaml'
-    ),
-    'supervised_beta_with_attn2': (
-        '/scratch/mcinerney.de/ehr-diagnosis-agent-output/wandb/run-20230616_004559-fn9b7mmz/files/ckpt_epoch=5_updates=218.pt',
-        '/scratch/mcinerney.de/ehr-diagnosis-agent-output/wandb/run-20230616_004559-fn9b7mmz/files/config.yaml'
-    ),
-    'supervised_beta_with_attn3': (
-        '/scratch/mcinerney.de/ehr-diagnosis-agent-output/wandb/run-20230616_122933-pp2dkyve/files/ckpt_epoch=16_updates=701.pt',
-        '/scratch/mcinerney.de/ehr-diagnosis-agent-output/wandb/run-20230616_122933-pp2dkyve/files/config.yaml'
-    ),
-    'supervised_beta_with_attn4': (
-        '/scratch/mcinerney.de/ehr-diagnosis-agent-output/wandb/run-20230617_213722-5zuox2g6/files/ckpt_epoch=41_updates=1853.pt',
-        '/scratch/mcinerney.de/ehr-diagnosis-agent-output/wandb/run-20230617_213722-5zuox2g6/files/config.yaml'
+    # 'supervised_dirichlet': (
+    #     '/work/frink/mcinerney.de/ehr-diagnosis-agent/ehr_diagnosis_agent/wandb/run-20230604_010745-6xacw8w1/files/ckpt_epoch=8_updates=986.pt',
+    #     '/work/frink/mcinerney.de/ehr-diagnosis-agent/ehr_diagnosis_agent/wandb/run-20230604_010745-6xacw8w1/files/config.yaml'
+    # ),
+    # 'supervised_beta': (
+    #     '/work/frink/mcinerney.de/ehr-diagnosis-agent/ehr_diagnosis_agent/wandb/run-20230614_095808-ov4baor2/files/ckpt_epoch=3_updates=34.pt',
+    #     '/work/frink/mcinerney.de/ehr-diagnosis-agent/ehr_diagnosis_agent/wandb/run-20230614_095808-ov4baor2/files/config.yaml'
+    # ),
+    # 'supervised_beta_with_bias': (
+    #     '/work/frink/mcinerney.de/ehr-diagnosis-agent/ehr_diagnosis_agent/wandb/run-20230614_134807-aaf16kc6/files/ckpt_epoch=23_updates=289.pt',
+    #     '/work/frink/mcinerney.de/ehr-diagnosis-agent/ehr_diagnosis_agent/wandb/run-20230614_134807-aaf16kc6/files/config.yaml'
+    # ),
+    # 'supervised_beta_with_attn': (
+    #     '/work/frink/mcinerney.de/ehr-diagnosis-agent/ehr_diagnosis_agent/wandb/run-20230614_183930-pj9ipm5h/files/ckpt_epoch=9_updates=92.pt',
+    #     '/work/frink/mcinerney.de/ehr-diagnosis-agent/ehr_diagnosis_agent/wandb/run-20230614_183930-pj9ipm5h/files/config.yaml'
+    # ),
+    # 'supervised_beta_with_attn2': (
+    #     '/scratch/mcinerney.de/ehr-diagnosis-agent-output/wandb/run-20230616_004559-fn9b7mmz/files/ckpt_epoch=5_updates=218.pt',
+    #     '/scratch/mcinerney.de/ehr-diagnosis-agent-output/wandb/run-20230616_004559-fn9b7mmz/files/config.yaml'
+    # ),
+    # 'supervised_beta_with_attn3': (
+    #     '/scratch/mcinerney.de/ehr-diagnosis-agent-output/wandb/run-20230616_122933-pp2dkyve/files/ckpt_epoch=16_updates=701.pt',
+    #     '/scratch/mcinerney.de/ehr-diagnosis-agent-output/wandb/run-20230616_122933-pp2dkyve/files/config.yaml'
+    # ),
+    # 'supervised_beta_with_attn4': (
+    #     '/scratch/mcinerney.de/ehr-diagnosis-agent-output/wandb/run-20230617_213722-5zuox2g6/files/ckpt_epoch=41_updates=1853.pt',
+    #     '/scratch/mcinerney.de/ehr-diagnosis-agent-output/wandb/run-20230617_213722-5zuox2g6/files/config.yaml'
+    # ),
+
+    'mixed_beta_with_attn': (
+        '/scratch/mcinerney.de/ehr-diagnosis-agent-output/wandb/run-20230712_215840-ktr1ito1/files/ckpt_epoch=16_updates=539.pt',
+        '/scratch/mcinerney.de/ehr-diagnosis-agent-output/wandb/run-20230712_215840-ktr1ito1/files/config.yaml'
     ),
 }
 actory_types = {
@@ -159,6 +164,8 @@ with jump_timestep_container:
 reward = 0
 i = 0
 terminated, truncated = False, False
+
+
 def display_state(observation, info, reward):
     st.subheader("Information")
     st.write(f'**timestep**: {i + 1}')
@@ -179,6 +186,133 @@ def display_state(observation, info, reward):
     if len(info['current_targets']) == 0:
         st.warning(
             'No targets so this is a dead environment! You can move to another instance.')
+
+
+def display_beta_actor_action(
+        observation, parameter_votes_info, parameters_info,
+        checkpoint_args, index, compare_to, index2):
+    # st.write('**concentration1**: {:.1f}'.format(parameters[0][index]))
+    # st.write('**concentration0**: {:.1f}'.format(parameters[1][index]))
+    concentration1_votes = parameter_votes_info['param_votes'][:, :, 0]
+    concentration0_votes = parameter_votes_info['param_votes'][:, :, 1]
+    context_strings = parameter_votes_info['context_strings']
+                        # TODO: add attention info if it exists
+    concentrations1 = torch.nn.functional.softplus(
+        concentration1_votes) + \
+        checkpoint_args.actor.value.beta_params.concentration_min
+    concentrations0 = torch.nn.functional.softplus(
+        concentration0_votes) + \
+        checkpoint_args.actor.value.beta_params.concentration_min
+    dist_means = [Beta(a, b).mean for a, b in zip(
+        concentrations1[index], concentrations0[index])]
+    dist_stddevs = [Beta(a, b).stddev for a, b in zip(
+        concentrations1[index], concentrations0[index])]
+    dist_means2 = [Beta(a, b).mean for a, b in zip(
+        concentrations1[index2], concentrations0[index2])] \
+        if compare_to else [None] * len(dist_means)
+    evidence_concentrations = list(zip(
+        context_strings, range(len(context_strings)), concentrations1[index],
+        concentrations0[index], dist_means, dist_stddevs, dist_means2))
+    def key(x): return -x[4]
+    if 'context_attn_weights' in parameters_info.keys() and sort_by_attn:
+        def key(x):
+            return -parameters_info['context_attn_weights'][index, x[1]]
+    evidence_concentrations = sorted(evidence_concentrations, key=key)
+    for ec in evidence_concentrations:
+        evidence = ("\"" + ec[0] + "\"") \
+            if ec[1] > 0 or observation['evidence_is_retrieved'] else \
+            'the report'
+        if compare_to:
+            attn_string = "" \
+                if 'context_attn_weights' not in parameters_info.keys() else \
+                ", attn={:.0f}%,{:.0f}%".format(
+                    parameters_info['context_attn_weights'][
+                        index, ec[1]].item() * 100,
+                    parameters_info['context_attn_weights'][
+                        index2, ec[1]].item() * 100,)
+            st.write('- (vote={}{:.1f}%{}) {}'.format(
+                '+' if ec[4] > ec[6] else '', (ec[4] - ec[6]) * 100,
+                attn_string, evidence))
+        else:
+            attn_string = "" \
+                if 'context_attn_weights' not in parameters_info.keys() else \
+                ", attn={:.0f}%".format(
+                parameters_info['context_attn_weights'][
+                    index, ec[1]].item() * 100)
+            st.write('- (vote={:.1f}%±{:.1f}{}) {}'.format(
+                ec[4] * 100, ec[5] * 100, attn_string, evidence))
+
+
+def display_diritchlet_actor_action(
+        observation, parameter_votes_info, parameters_info, checkpoint_args,
+        index, compare_to, index2, diagnosis):
+    concentration = parameters_info['params'][0]
+    st.write('**average of all concentrations**: {:.1f}'.format(
+        concentration.mean()))
+    st.write('**{} concentration**: {:.1f}'.format(
+        diagnosis, concentration[index]))
+    concentration_votes = parameter_votes_info['param_votes'][:, :, 0]
+    context_strings = parameter_votes_info['context_strings']
+    # TODO: add attention info if it exists
+    concentrations = torch.nn.functional.softplus(concentration_votes) + \
+        checkpoint_args.actor.value.dirichlet_params.concentration_min
+    evidence_concentrations = list(zip(
+        context_strings, range(len(context_strings)), concentrations[index],
+        concentrations[index2]))
+    def key(x): return -x[2]
+    if 'context_attn_weights' in parameters_info.keys() and sort_by_attn:
+        def key(x):
+            return -parameters_info['context_attn_weights'][index, x[1]]
+    evidence_concentrations = sorted(evidence_concentrations, key=key)
+    for ec in evidence_concentrations:
+        evidence = ("\"" + ec[0] + "\"") \
+            if ec[1] > 0 or observation['evidence_is_retrieved'] else \
+            'the report'
+        if compare_to:
+            attn_string = "" \
+                if 'context_attn_weights' not in parameters_info.keys() else \
+                ", attn={:.0f}%,{:.0f}%".format(
+                    parameters_info[
+                        'context_attn_weights'][index, ec[1]].item() * 100,
+                    parameters_info[
+                        'context_attn_weights'][index2, ec[1]].item() * 100)
+            st.write('- (vote={}{:.1f}{}) {}'.format(
+                '+' if ec[2] > ec[3] else '', ec[2] - ec[3], attn_string,
+                evidence))
+        else:
+            attn_string = "" \
+                if 'context_attn_weights' not in parameters_info.keys() else \
+                ", attn={:.0f}%".format(
+                    parameters_info[
+                        'context_attn_weights'][index, ec[1]].item() * 100)
+            st.write('- (vote={:.1f}{}) {}'.format(
+                ec[2], attn_string, evidence))
+    return key
+
+
+def display_action_evidence(
+        observation, parameter_votes_info, parameters_info, checkpoint_args,
+        diagnosis, rating, stddev, index, compare_to, diagnosis2, rating2,
+        stddev2, index2):
+    if compare_to:
+        st.write('### {} - {} (delta={}{:.1f}%)'.format(
+            diagnosis, diagnosis2, '+' if rating < rating2 else '',
+            (rating - rating2) * 100))
+    else:
+        st.write('### {} (rating={:.1f}%±{:.1f})'.format(
+            diagnosis, rating * 100, stddev * 100))
+    if isinstance(get_actor(actor_checkpoint), InterpretableDirichletActor):
+        display_diritchlet_actor_action(
+            observation, parameter_votes_info, parameters_info,
+            checkpoint_args, index, compare_to, index2, diagnosis)
+    elif isinstance(get_actor(actor_checkpoint), InterpretableBetaActor):
+        return display_beta_actor_action(
+            observation, parameter_votes_info, parameters_info,
+            checkpoint_args, index, compare_to, index2)
+    elif isinstance(get_actor(actor_checkpoint), InterpretableNormalActor):
+        raise NotImplementedError
+
+
 while not (terminated or truncated):
     with container.container():
         if i not in st.session_state['episode']['steps'].keys():
@@ -232,77 +366,32 @@ while not (terminated or truncated):
                     return '({:.1f}%±{:.1f}) {}'.format(r * 100, stddev * 100, d)
                 with c1:
                     diagnosis_index = st.radio(
-                        'Choose a diagnosis to see what impacted the score.',
+                        'Choose an option to see what impacted the score.',
                         range(len(potential_diagnoses_ratings_indices)),
                         format_func=format_func,
                         key=f'diagnosis to view {i}')
-                diagnosis, rating, stddev, index = potential_diagnoses_ratings_indices[
-                    diagnosis_index]
+                    compare_to = st.checkbox(
+                        'Compare option with...', key=f'compare to {i}')
+                    diagnosis, rating, stddev, index = \
+                        potential_diagnoses_ratings_indices[
+                            diagnosis_index]
+                    if compare_to:
+                        diagnosis_index2 = st.radio(
+                            'Choose another option to compare to.',
+                            range(len(potential_diagnoses_ratings_indices)),
+                            format_func=format_func,
+                            key=f'diagnosis to compare to {i}')
+                        diagnosis2, rating2, stddev2, index2 = \
+                            potential_diagnoses_ratings_indices[
+                                diagnosis_index2]
+                    else:
+                        diagnosis2, rating2, stddev2, index2 = \
+                            None, None, None, None
                 with c2:
-                    st.write('### {} (rating={:.1f}%±{:.1f})'.format(
-                        diagnosis, rating * 100, stddev * 100))
-                    if isinstance(get_actor(actor_checkpoint), InterpretableDirichletActor):
-                        concentration = parameters_info['params'][0]
-                        st.write(
-                            '**average of all concentrations**: {:.1f}'.format(concentration.mean()))
-                        st.write(
-                            '**{} concentration**: {:.1f}'.format(diagnosis, concentration[index]))
-                        concentration_votes = parameter_votes_info['param_votes'][:, :, 0]
-                        context_strings = parameter_votes_info['context_strings']
-                        # TODO: add attention info if it exists
-                        concentrations = torch.nn.functional.softplus(concentration_votes) + \
-                            checkpoint_args.actor.value.dirichlet_params.concentration_min
-                        evidence_concentrations = list(zip(
-                            context_strings, range(len(context_strings)), concentrations[index]))
-
-                        def key(x): return -x[2]
-                        if 'context_attn_weights' in parameters_info.keys() and sort_by_attn:
-                            def key(
-                                x): return -parameters_info['context_attn_weights'][index, x[1]]
-                        evidence_concentrations = sorted(
-                            evidence_concentrations, key=key)
-                        for ec in evidence_concentrations:
-                            evidence = ("\"" + ec[0] + "\"") \
-                                if ec[1] > 0 or observation['evidence_is_retrieved'] else 'the report'
-                            attn_string = "" if 'context_attn_weights' not in parameters_info.keys() else \
-                                ", attn={:.0f}%".format(
-                                    parameters_info['context_attn_weights'][index, ec[1]].item() * 100)
-                            st.write(
-                                '- (vote={:.1f}{}) {}'.format(ec[2], attn_string, evidence))
-                    elif isinstance(get_actor(actor_checkpoint), InterpretableBetaActor):
-                        # st.write('**concentration1**: {:.1f}'.format(parameters[0][index]))
-                        # st.write('**concentration0**: {:.1f}'.format(parameters[1][index]))
-                        concentration1_votes = parameter_votes_info['param_votes'][:, :, 0]
-                        concentration0_votes = parameter_votes_info['param_votes'][:, :, 1]
-                        context_strings = parameter_votes_info['context_strings']
-                        # TODO: add attention info if it exists
-                        concentrations1 = torch.nn.functional.softplus(concentration1_votes) + \
-                            checkpoint_args.actor.value.beta_params.concentration_min
-                        concentrations0 = torch.nn.functional.softplus(concentration0_votes) + \
-                            checkpoint_args.actor.value.beta_params.concentration_min
-                        dist_means = [Beta(a, b).mean for a, b in zip(
-                            concentrations1[index], concentrations0[index])]
-                        dist_stddevs = [Beta(a, b).stddev for a, b in zip(
-                            concentrations1[index], concentrations0[index])]
-                        evidence_concentrations = list(zip(
-                            context_strings, range(len(context_strings)), concentrations1[index], concentrations0[index], dist_means, dist_stddevs))
-
-                        def key(x): return -x[4]
-                        if 'context_attn_weights' in parameters_info.keys() and sort_by_attn:
-                            def key(
-                                x): return -parameters_info['context_attn_weights'][index, x[1]]
-                        evidence_concentrations = sorted(
-                            evidence_concentrations, key=key)
-                        for ec in evidence_concentrations:
-                            evidence = ("\"" + ec[0] + "\"") \
-                                if ec[1] > 0 or observation['evidence_is_retrieved'] else 'the report'
-                            attn_string = "" if 'context_attn_weights' not in parameters_info.keys() else \
-                                ", attn={:.0f}%".format(
-                                    parameters_info['context_attn_weights'][index, ec[1]].item() * 100)
-                            st.write(
-                                '- (vote={:.1f}%±{:.1f}{}) {}'.format(ec[4] * 100, ec[5] * 100, attn_string, evidence))
-                    elif isinstance(get_actor(actor_checkpoint), InterpretableNormalActor):
-                        raise NotImplementedError
+                    display_action_evidence(
+                        observation, parameter_votes_info, parameters_info,
+                        checkpoint_args, diagnosis, rating, stddev, index,
+                        compare_to, diagnosis2, rating2, stddev2, index2)
             else:
                 raise Exception
             action_submitted = st.button('Submit Action', key=f'submit {i}')
